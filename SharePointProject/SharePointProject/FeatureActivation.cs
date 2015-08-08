1dbc4b66740cc968c82d20928c2fdfd0413c7a8e
michal_urbanski <michal_urbanski@windowslive.com>
2015-08-08 22:51:09 +0200

    Creates web part page in site and it's copy

---
+++ SharePointProject/SharePointProject/FeatureActivation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SharePointProject
+{
+    /// <summary>
+    /// Actions performed during feature activation
+    /// </summary>
+    internal class FeatureActivation
+    {
+        public void CreateWebPartPage(SPWeb web, string pageFileName, string templateFileName)
+        {
+            int lcid = web.Locale.LCID;
+            string hive = SPUtility.GetGenericSetupPath(string.Format("TEMPLATE\\{0}\\STS\\DOCTEMP\\SMARTPGS\\", lcid));
+            FileStream fileStream = new FileStream(hive + templateFileName, FileMode.Open);
+
+            web.Files.Add(pageFileName, fileStream, true); // Last parameters overwrites created page
+        }
+
+        public void CopySitePage(SPWeb web, string sourceFileName, string destinationFileName)
+        {
+            SPFile file = web.GetFile(sourceFileName);
+            if (!file.Exists)
+            {
+                throw new ArgumentException("Incorrect file name " + sourceFileName);
+            }
+
+            file.CopyTo(destinationFileName, true);
+        }
+    }
+}
---
+++ SharePointProject/SharePointProject/Features/WebFeature/WebFeature.EventReceiver.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+
+namespace SharePointProject.Features.WebFeature
+{
+    /// <summary>
+    /// This class handles events raised during feature activation, deactivation, installation, uninstallation, and upgrade.
+    /// </summary>
+    /// <remarks>
+    /// The GUID attached to this class may be used during packaging and should not be modified.
+    /// </remarks>
+
+    [Guid("3c98fd33-4f90-48bc-b82d-bd0d87ecf34b")]
+    public class WebFeatureEventReceiver : SPFeatureReceiver
+    {
+        // Uncomment the method below to handle the event raised after a feature has been activated.
+
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            SPWeb web = properties.Feature.Parent as SPWeb; 
+            
+            FeatureActivation featureActivation = new FeatureActivation();
+            featureActivation.CreateWebPartPage(web, "newPage.aspx", "spstd1.aspx");
+
+            featureActivation.CopySitePage(web, "newPage.aspx", "newPageCopy.aspx");
+
+        }
+
+
+        // Uncomment the method below to handle the event raised before a feature is deactivated.
+
+        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised after a feature has been installed.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is uninstalled.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Uncomment the method below to handle the event raised when a feature is upgrading.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
