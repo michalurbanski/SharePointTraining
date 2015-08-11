39500cca229b97ac16479065a18d844f71345d57
michal_urbanski <michal_urbanski@windowslive.com>
2015-08-11 23:07:55 +0200

    Applies custom branding for all sites provisioned after package installation

---
+++ SharePointProject/SharePointProject/ChildSiteInit/ChildSiteInit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace SharePointProject.ChildSiteInit
+{
+    /// <summary>
+    /// Web Events. Applies branding to newly created webs. 
+    /// </summary>
+    public class ChildSiteInit : SPWebEventReceiver
+    {
+        /// <summary>
+        /// A site was provisioned.
+        /// </summary>
+        public override void WebProvisioned(SPWebEventProperties properties)
+        {
+            //base.WebProvisioned(properties);
+
+            SPWeb web = properties.Web; 
+            SPWeb rootWeb = web.Site.RootWeb;
+
+            web.MasterUrl = rootWeb.MasterUrl;
+            web.CustomMasterUrl = rootWeb.CustomMasterUrl; 
+            web.AlternateCssUrl = rootWeb.AlternateCssUrl;
+            web.SiteLogoUrl = rootWeb.SiteLogoUrl;
+            web.Update(); 
+        }
+    }
+}
--- SharePointProject/SharePointProject/Features/SiteFeature/SiteFeature.feature
+++ SharePointProject/SharePointProject/Features/SiteFeature/SiteFeature.feature
@@ -2,5 +2,6 @@
 <feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="3b789bf7-f986-4f60-8f3e-1fdb77bafbc6" featureId="3b789bf7-f986-4f60-8f3e-1fdb77bafbc6" imageUrl="" receiverAssembly="$SharePoint.Project.AssemblyFullName$" receiverClass="$SharePoint.Type.75367ac3-9f91-4543-addf-0cc385c4d0c7.FullName$" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SiteFeature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
   <projectItems>
     <projectItemReference itemId="cd3b7a2f-4e78-44cc-98c3-06e79b4241d3" />
+    <projectItemReference itemId="33007b9a-155b-4cf4-aa2c-647425a94cf8" />
   </projectItems>
 </feature>
